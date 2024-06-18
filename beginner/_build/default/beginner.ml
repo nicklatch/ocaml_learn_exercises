b4e9a8bce465ebe4f5ea687d546416761e49bfa3
Nick Latcham <nick.latcham@proton.me>
2024-06-18 15:31:34 -0500

    test: add tests and new functions

--- beginner/_build/default/beginner.ml
+++ beginner/_build/default/beginner.ml
@@ -3,8 +3,18 @@ let rec last = function
   | [ x ] -> Some x
   | _ :: t -> last t
 
-let last_two = function
+let rec last_two = function
   | [] | [ _ ] -> None
   | [ h; t ] -> Some (h, t)
-  | _ -> failwith "Testing"
-(*  | _ :: t -> last_two t *)
+  | _ :: t -> last_two t
+
+let rec nth_el el = function
+  | [] -> None
+  | h :: t -> if el = 0 then Some h else nth_el (el - 1) t
+
+let length list =
+  let rec aux acc = function
+    | [] -> acc
+    | _ :: t -> aux (acc + 1) t
+  in
+  aux 0 list

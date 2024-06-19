6cf1fd146143f1c7856285dca567a97fe7406d3e
Nick Latcham <nick.latcham@proton.me>
2024-06-19 10:10:30 -0500

    refactor: remove util functions
    remove util functions that were moved to Exercises.Utils module and
    import them

--- test/beginner_test.ml
+++ test/beginner_test.ml
@@ -1,26 +1,6 @@
 open OUnit2
 open Exercises.Beginner
-
-let ( -- ) start stop =
-  let rec aux f t l = if f > t then l else aux f (t - 1) (t :: l) in
-  aux start stop []
-
-let string_of_int_option = function
-  | None -> "None"
-  | Some v -> "Some " ^ string_of_int v
-
-let string_of_int_int_option = function
-  | None -> "None"
-  | Some (v1, v2) -> "Some (" ^ string_of_int v1 ^ ", " ^ string_of_int v2 ^ ")"
-
-let string_of_list string_of list =
-  let rec aux acc = function
-    | [] -> "[]"
-    | [ x ] -> string_of x
-    | h :: t -> aux (acc ^ " " ^ string_of h ^ ";") t
-  in
-  let contents = aux "" list in
-  "[" ^ contents ^ "]"
+open Exercises.Utils
 
 let ae test_name expected got ~printer =
   test_name >:: fun _ -> assert_equal expected got ~printer
